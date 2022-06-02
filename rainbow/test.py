52f3509981bbdf930e2bab7330af08b008bded16
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2022-06-02 14:32:28 -0400

    Update test file

--- rainbow/test.py
+++ rainbow/test.py
@@ -4,8 +4,15 @@
 import parser
 
 chromatogram = parser.read("../data/caffeine.D")
+print("X values")
 print(chromatogram.get_X())
+print("Y values")
 print(chromatogram.get_Y())
+print("Y labels")
 print(chromatogram.get_Ylabels())
+print("Detectors")
 print(chromatogram.get_detectors())
+print("Metadata")
 print(chromatogram.get_metadata())
+print("Sample traces")
+print(chromatogram.extract_traces("UV", [210, 212]))
