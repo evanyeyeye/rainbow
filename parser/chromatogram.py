35a7b77fccd59dd71a67a7d1404e6cc7ae0c1c16
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2022-06-02 13:33:37 -0400

    Add parser interface

---
+++ parser/chromatogram.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Chromatogram(ABC):
+
+    @abstractmethod 
+    def get_X():
+        pass
+    
+    @abstractmethod 
+    def get_Y():
+        pass
+
+    @abstractmethod 
+    def get_Ylabels():
+        pass
+
+    @abstractmethod 
+    def get_detectors():
+        pass
+
+    @abstractmethod
+    def get_metadata():
+        pass
+
