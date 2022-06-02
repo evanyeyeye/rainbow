7d70470babb0c1dd964226654a1d0e181919ba47
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2022-06-02 13:34:30 -0400

    Add parsing for agilent UV

---
+++ parser/agilent.py
@@ -0,0 +1,99 @@
+from chromatogram import Chromatogram 
+import numpy as np
+import struct
+
+
+class AgilentUV(Chromatogram):
+
+    def __init__(self, filepath):
+        self.parse(filepath)
+
+    def read_string(self, f, offset):
+        f.seek(offset)
+        length = struct.unpack("<B", f.read(1))[0] * 2
+        return f.read(length)[::2].decode()
+
+    def extract_metadata(self, f):
+
+        offsets = {
+            "notebook": 0x35A,
+            "date": 0x957,
+            "method": 0xA0E,
+            "unit": 0xC15,
+            "datatype": 0xC40,
+            "position": 0xFD7
+        }
+
+        metadata = {}
+        metadata["vendor"] = "agilent"
+        metadata["instrument"] = "HPLC"
+
+        for key, value in offsets.items():
+            metadata[key] = self.read_string(f, value)
+        
+        return metadata
+
+    # Completely parse file at the start
+    # so future operations do not require the file
+    def parse(self, filepath):
+
+        offsets = {
+            "number of data points": 0x116,
+            "start of data body": 0x1000
+        }
+
+        f = open(filepath, "rb")
+
+        # Sets the total number of x-axis values (or rows) for the array.
+        f.seek(offsets["number of data points"])
+        num_data_points = struct.unpack(">i", f.read(4))[0]
+        print(num_data_points)
+
+        times = np.zeros(num_data_points, np.uint64)
+
+        # Get the number of wavelengths using the header for the first data segment.
+        f.seek(offsets["start of data body"] + 8)
+        wavelength_range = tuple(num // 20 for num in struct.unpack("<HHH", f.read(6)))
+        wavelengths = np.arange(wavelength_range[0], wavelength_range[1] + 1, wavelength_range[2])
+        num_wavelengths = wavelengths.size
+
+        # Extract absorbance data from each data segment.
+        absorbances = np.zeros((num_data_points, num_wavelengths), np.int64)
+        f.seek(offsets["start of data body"])
+        for i in range(num_data_points):
+            # Read in header information.
+            f.read(4)
+            times[i] = struct.unpack("<I", f.read(4))[0]
+            f.read(14)
+        
+            # If next value is a delta, add it to the last integer value (accumulating).
+            accum = 0 
+            for j in range(num_wavelengths):
+                check_val = struct.unpack('<h', f.read(2))[0]
+                if check_val == -0x8000:
+                    accum = struct.unpack('<i', f.read(4))[0]
+                else: accum += check_val
+                absorbances[i, j] = accum
+
+        self.X = times 
+        self.Y = np.array([absorbances])
+        self.Ylabels = np.array([wavelengths])
+        self.detectors = ["UV"]
+        self.metadata = self.extract_metadata(f)
+
+        f.close()
+
+    def get_X(self):
+        return self.X
+    
+    def get_Y(self):
+        return self.Y
+
+    def get_Ylabels(self):
+        return self.Ylabels 
+
+    def get_detectors(self):
+        return self.detectors
+
+    def get_metadata(self):
+        return self.metadata
