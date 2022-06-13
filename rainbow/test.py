f3771cfa0bf26b8f9a480d904c733f01234a8060
evanyeyeye <14984764+evanyeyeye@users.noreply.github.com>
2022-06-13 15:34:11 -0400

    Modify temporary test.py

--- rainbow/test.py
+++ rainbow/test.py
@@ -11,28 +11,31 @@ data_folders = [
     "caye_nmr_test_mix.D"
 ]
 
-chromatogram = parser.read("../data/" + data_folders[2])
+chromatogram = parser.read("../data/" + data_folders[1])
 
+print("X labels:")
 print(chromatogram.xlabels)
+print()
+
+print("Y labels:")
 print(chromatogram.ylabels)
+print()
+
+print("Data array:")
 print(chromatogram.data)
+print()
+
+print("Metadata:")
 print(chromatogram.metadata)
+print()
 
+print("Sample traces:")
+traces = chromatogram.extract_traces('UV')
+print(traces)
 
-# print("X values")
-# print(chromatogram.get_X())
-# print("Y values")
-# print(chromatogram.get_Y())
-# print("Y labels")
-# print(chromatogram.get_Ylabels())
-# print("Detectors")
-# print(chromatogram.get_detectors())
-# print("Metadata")
-# print(chromatogram.get_metadata())
-# print("Sample traces")
-# print(chromatogram.extract_traces("UV", [210, 212]))
-# chromatogram.export_csv("test.csv", "UV", [210, 212])
-# chromatogram.plot("UV", 210)
+chromatogram.export_csv("test.csv", 'UV')
+chromatogram.plot('UV', 210)
+
 # a=0
 # e = chromatogram.data['FID']
 # f = open("caye_nmr_test_mix.csv").readlines()[1:]
